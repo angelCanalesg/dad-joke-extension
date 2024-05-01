323cca50ffb706a6d0ddb976cf591d55e4950adc
Tom Roberts <tomroberts@teamitg.com>
2024-05-01 08:53:39 +0100

    prod ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React, {useCallback, useState} from 'react';
 import styles from './App.module.scss';
+import useJoke from "./useJoke";
 
 function App() {
 
-  const [joke, setJoke] = useState<string>('');
-
-  const getJoke = useCallback(async () => {
-    setJoke('Loading...');
-
-    const res = await fetch('https://icanhazdadjoke.com/', {
-      headers: {
-        Accept: 'application/json'
-      }
-    });
-
-    setJoke((await res.json()).joke);
-  }, []);
-
+  const {joke, getJoke} = useJoke();
 
   return (
     <div className={styles.container}>
-        <button onClick={getJoke} className={styles.button}>Tell me a Dad joke</button>
+        <button onClick={getJoke} className={styles.button}>
+          Tell me a Dad joke
+        </button>
+
         {joke && <p className={styles.joke}>{joke}</p>}
     </div>
   );
